f5125cd062d773645a3ada641d320da56c92dcf5
RetaxMaster <retaxmaster@hotmail.com>
2023-01-21 11:16:39 -0600

    Refactor de la calculadora

--- Practico/Calculadora/calculadora.go
+++ Practico/Calculadora/calculadora.go
@@ -8,48 +8,58 @@ import (
 	"strings"
 )
 
-func main() {
+type calc struct {
+}
 
-	// Lectura de la entrada del usuario
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	operacion := scanner.Text()
+func (calc) operate(entrada string, operador string) int {
 
-	fmt.Println(operacion)
-
-	operador := "*"
-
-	valores := strings.Split(operacion, operador)
-	fmt.Println(valores)
-	fmt.Println(valores[0] + valores[1])
-
-	operador1, err1 := strconv.Atoi(valores[0])
-
-	if err1 != nil {
-		fmt.Println(err1)
-	} else {
-		fmt.Println(operador1)
-	}
-
-	operador2, _ := strconv.Atoi(valores[1])
+	entradaLimpia := strings.Split(entrada, operador)
+	operador1 := parsear(entradaLimpia[0])
+	operador2 := parsear(entradaLimpia[1])
 
 	switch operador {
 
 	case "+":
 		fmt.Println(operador1 + operador2)
+		return operador1 + operador2
 
 	case "-":
 		fmt.Println(operador1 - operador2)
+		return operador1 - operador2
 
 	case "*":
 		fmt.Println(operador1 * operador2)
+		return operador1 * operador2
 
 	case "/":
 		fmt.Println(operador1 / operador2)
+		return operador1 / operador2
 
 	default:
 		fmt.Println(operador, "No está soportado")
+		return 0
 
 	}
+
+}
+
+func parsear(entrada string) int {
+	operador, _ := strconv.Atoi(entrada)
+	return operador
+}
+
+func leerEntrada() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func main() {
+
+	entrada := leerEntrada()
+	operador := leerEntrada()
+
+	c := calc{}
+	fmt.Println(c.operate(entrada, operador))
 
 }
