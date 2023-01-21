ea81480cdee9d905f429fa41eff098bcb8035f96
RetaxMaster <retaxmaster@hotmail.com>
2023-01-21 11:03:52 -0600

    Adicion de switch

--- Practico/Calculadora/calculadora.go
+++ Practico/Calculadora/calculadora.go
@@ -16,7 +16,10 @@ func main() {
 	operacion := scanner.Text()
 
 	fmt.Println(operacion)
-	valores := strings.Split(operacion, "+")
+
+	operador := "*"
+
+	valores := strings.Split(operacion, operador)
 	fmt.Println(valores)
 	fmt.Println(valores[0] + valores[1])
 
@@ -30,6 +33,23 @@ func main() {
 
 	operador2, _ := strconv.Atoi(valores[1])
 
-	fmt.Println(operador1 + operador2)
+	switch operador {
+
+	case "+":
+		fmt.Println(operador1 + operador2)
+
+	case "-":
+		fmt.Println(operador1 - operador2)
+
+	case "*":
+		fmt.Println(operador1 * operador2)
+
+	case "/":
+		fmt.Println(operador1 / operador2)
+
+	default:
+		fmt.Println(operador, "No está soportado")
+
+	}
 
 }
