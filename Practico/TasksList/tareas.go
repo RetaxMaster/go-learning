8662c3afe4914e2c60054b630b7e5c035753cd8b
RetaxMaster <retaxmaster@hotmail.com>
2023-01-21 13:07:00 -0600

    Adicion de for y for range

--- Practico/TasksList/tareas.go
+++ Practico/TasksList/tareas.go
@@ -56,10 +56,14 @@ func main() {
 		},
 	}
 
-	fmt.Println(lista.tasks[0])
 	lista.agregarALista(t3)
-	fmt.Println(len(lista.tasks))
-	lista.eliminarDeLista(1)
-	fmt.Println(len(lista.tasks))
+
+	for i := 0; i < len(lista.tasks); i++ {
+		fmt.Println("Index", i, "Nombre", lista.tasks[i].nombre)
+	}
+
+	for index, tarea := range lista.tasks {
+		fmt.Println("Index", index, "Nombre", tarea.nombre)
+	}
 
 }
