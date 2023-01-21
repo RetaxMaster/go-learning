92bfdb8960f0f900f4f0b83e0cdd44583f347947
RetaxMaster <retaxmaster@hotmail.com>
2023-01-21 13:01:21 -0600

    Adicion de slices

--- Practico/TasksList/tareas.go
+++ Practico/TasksList/tareas.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type taskList struct {
+	tasks []*task
+}
+
+func (t *taskList) agregarALista(tl *task) {
+	t.tasks = append(t.tasks, tl)
+}
+
+func (t *taskList) eliminarDeLista(index int) {
+	// Reemplaza todos los elementos por todos los elementos antes del indice y todos los elementos después del indice (aisla al que se elimina y simplemente no lo agrega)
+	t.tasks = append(t.tasks[:index], t.tasks[index+1:]...)
+}
+
 type task struct {
 	nombre      string
 	descripcion string
@@ -22,17 +35,31 @@ func (t *task) actualizarNombre(nombre string) {
 
 func main() {
 
-	t := &task{
+	t1 := &task{
 		nombre:      "Completar mi curso de Go",
 		descripcion: "Completar mi curso de Go de Platzi en esta semana",
 	}
 
-	fmt.Printf("%+v\n", t)
+	t2 := &task{
+		nombre:      "Completar mi curso de Python",
+		descripcion: "Completar mi curso de Python de Platzi en esta semana",
+	}
 
-	t.marcarCompleto()
-	t.actualizarNombre("Finalizar mi curso de Go")
-	t.actualizarDescripcion("Completar mi curso cuanto antes")
+	t3 := &task{
+		nombre:      "Completar mi curso de NodeJS",
+		descripcion: "Completar mi curso de NodeJS de Platzi en esta semana",
+	}
 
-	fmt.Printf("%+v\n", t)
+	lista := &taskList{
+		tasks: []*task{
+			t1, t2,
+		},
+	}
+
+	fmt.Println(lista.tasks[0])
+	lista.agregarALista(t3)
+	fmt.Println(len(lista.tasks))
+	lista.eliminarDeLista(1)
+	fmt.Println(len(lista.tasks))
 
 }
