a79aae89d2d6af5c17e21696ba2d07497d5c0fba
RetaxMaster <retaxmaster@hotmail.com>
2023-01-22 11:18:11 -0600

    Adicion de verbos HTTP

--- Practico/WebServer/main.go
+++ Practico/WebServer/main.go
@@ -4,8 +4,8 @@ func main() {
 
 	server := NewServer(":3000")
 
-	server.Handle("/", HandleRoot)
-	server.Handle("/api", server.AddMiddleware(HandleHome, CheckAuth(), Logging()))
+	server.Handle("GET", "/", HandleRoot)
+	server.Handle("POST", "/api", server.AddMiddleware(HandleHome, CheckAuth(), Logging()))
 
 	server.Listen()
 
